fcd05da37e2c51b9f3713ce327cc5209d6ed2554
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-04-06 15:38:59 -0600

    Create duplicate header component for narrow screen

---
+++ src/components/NavNarrowScreen.js
@@ -0,0 +1,27 @@
+import styles from '../styles/NavNarrowScreen.module.css'
+
+export default function Header() {
+  return(
+    <div
+      className={styles.header}
+    >
+      <div    
+        className={styles.headerContent}
+      >
+        <h1
+          className={styles.logo}
+        >
+          <img
+            src="/florasync.png"
+            className={styles.logoImage}
+          />
+        </h1>
+        <p
+          className={styles.slogan}
+        >
+          We're making plants love technology.
+        </p>
+      </div>
+    </div>
+  )
+}
