18266bca37f3aa6c5658c57099b642f034e88cd3
Schuyler Ankele <schuyler.ankele@gmail.com>
2021-04-15 00:23:10 -0700

    Use Bootsrap 5 in header and change cols to rows

--- src/components/NavWideScreen.js
+++ src/components/NavWideScreen.js
@@ -1,24 +1,15 @@
-import styles from '../styles/NavWideScreen.module.css'
+import styles from '../styles/NavWideScreen.module.scss'
 
-export default function Header() {
+
+export default function NavWideScreen() {
   return(
-    <div
-      className={styles.header}
-    >
-      <div    
-        className={styles.headerContent}
-      >
-        <h1
-          className={styles.logo}
-        >
-          <img
-            src="/florasync.png"
-            className={styles.logoImage}
-          />
+    <div className={`${styles.header} col-6 d-none d-md-block`}>
+      <div className={styles.headerContent}>
+        <h1 className={styles.logo}>
+          <img src="/florasync.png"
+               className={styles.logoImage}/>
         </h1>
-        <p
-          className={styles.slogan}
-        >
+        <p className={styles.slogan}>
           We're making plants love technology.
         </p>
       </div>
